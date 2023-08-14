715bce362759f808a7f341d6a03e4fada3c58bd5
Joseph ITAKALA <josephitakala18@gmail.com>
2023-08-14 14:44:07 +0100

    feat : fix des bugs lié aux boutons digits, cas du bouton 0 et de la virgule

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const chainePremierOperande = [];
 const screenReset = () => {
   const reset = document.getElementById('reset');
 
-  reset.addEventListener('click', function() {
+  reset.addEventListener('click', function () {
     calcul.textContent = '';
     input.value = '';
     chainePremierOperande.length = 0;
@@ -23,7 +23,7 @@ const screenReset = () => {
 
   const clear = document.getElementById('clear');
 
-  clear.addEventListener('click', function() {
+  clear.addEventListener('click', function () {
     input.value = '';
     chainePremierOperande.length = 0;
   });
@@ -32,7 +32,7 @@ screenReset();
 
 //Bouton +/-
 const plusoumoins = document.getElementById('plusoumoins');
-plusoumoins.addEventListener('click', function() {
+plusoumoins.addEventListener('click', function () {
   if (input.value.length >= 1 && !input.value.includes('-')) {
     input.value = '-' + input.value;
   } else {
@@ -46,34 +46,54 @@ btns.forEach((btn) => {
     e.preventDefault();
     switch (e.target.dataset.value) {
       case '1':
-        input.value += '1';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '1')
+          : (input.value += '1');
         break;
       case '2':
-        input.value += '2';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '2')
+          : (input.value += '2');
         break;
       case '3':
-        input.value += '3';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '3')
+          : (input.value += '3');
         break;
       case '4':
-        input.value += '4';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '4')
+          : (input.value += '4');
         break;
       case '5':
-        input.value += '5';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '5')
+          : (input.value += '5');
         break;
       case '6':
-        input.value += '6';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '6')
+          : (input.value += '6');
         break;
       case '7':
-        input.value += '7';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '7')
+          : (input.value += '7');
         break;
       case '8':
-        input.value += '8';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '8')
+          : (input.value += '8');
         break;
       case '9':
-        input.value += '9';
+        input.value.indexOf('0') === 0 && input.value[1] !== '.'
+          ? (input.value = '9')
+          : (input.value += '9');
         break;
       case '0':
-        if (input.value.length >= 1) {
+        if (input.value.length === 0) {
+          input.value = '0';
+        } else if (input.value.length >= 1 && input.value !== '0') {
           input.value += '0';
         }
         break;
@@ -95,8 +115,8 @@ const minusOperator = document.getElementById('minus');
 const addOperator = document.getElementById('plus');
 
 //Fonction addition
-const plusOperator1 = function() {
-  addOperator.addEventListener('click', function(event) {
+const plusOperator1 = function () {
+  addOperator.addEventListener('click', function (event) {
     event.preventDefault();
     calcul.textContent = '';
     const lastValue = chainePremierOperande.pop();
@@ -117,8 +137,8 @@ const plusOperator1 = function() {
 plusOperator1();
 
 // Fonction soustraction
-const minusOperator1 = function() {
-  minusOperator.addEventListener('click', function(event) {
+const minusOperator1 = function () {
+  minusOperator.addEventListener('click', function (event) {
     event.preventDefault();
     calcul.textContent = '';
     const lastValue = chainePremierOperande.pop();
@@ -139,8 +159,8 @@ const minusOperator1 = function() {
 minusOperator1();
 
 // Fonction multiplication
-const multiplicationOperator1 = function() {
-  multiplicationOperator.addEventListener('click', function(event) {
+const multiplicationOperator1 = function () {
+  multiplicationOperator.addEventListener('click', function (event) {
     event.preventDefault();
     calcul.textContent = '';
     const lastValue = chainePremierOperande.pop();
@@ -161,8 +181,8 @@ const multiplicationOperator1 = function() {
 multiplicationOperator1();
 
 // Fonction division
-const dividebyOperator1 = function() {
-  dividebyOperator.addEventListener('click', function(event) {
+const dividebyOperator1 = function () {
+  dividebyOperator.addEventListener('click', function (event) {
     event.preventDefault();
     calcul.textContent = '';
     const lastValue = chainePremierOperande.pop();
@@ -184,7 +204,7 @@ dividebyOperator1();
 
 // Fonction calcul ==> premierOperande + operator + deuxièmeOperande = resultat
 const equals = document.getElementById('equals');
-equals.addEventListener('click', function(event) {
+equals.addEventListener('click', function (event) {
   event.preventDefault();
   if (input.value.length >= 1) {
     chainePremierOperande.push(input.value);
@@ -197,7 +217,7 @@ equals.addEventListener('click', function(event) {
 
 //Fonction calcul de pourcentage d'une valeur
 const percentage = document.getElementById('percentage');
-percentage.addEventListener('click', function(event) {
+percentage.addEventListener('click', function (event) {
   event.preventDefault();
   if (input.value.length >= 1) {
     const premierOperande = Number(input.value);
